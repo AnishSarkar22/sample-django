72f8e4dd319e7ec037d195a89d37d841bb51bed0
Anish Sarkar <anish16916@gmail.com>
2025-01-31 03:12:23 +0530

    laying the pipeline and creating index.html

--- sampleapp/sampleapp/views.py
+++ sampleapp/sampleapp/views.py
@@ -1,10 +1,17 @@
 # I have created this file 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Home")
+    # return HttpResponse("Home")
+    # params = {'name':'Anish', 'place':'Mars'}
+    return render(request, 'index.html')
 
 def removepunc(request):
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    # Analyze the text
     return HttpResponse("remove punc")
 
 def capfirst(request):
